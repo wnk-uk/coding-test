3c55e2ac47227470013687688c7740cca947d3e0
wnk-uk <dailycoding0211@gmail.com>
2024-02-22 20:02:18 +0900

    init

---
+++ src/Array/PrimeNumber.java
@@ -0,0 +1,29 @@
+package Array;
+
+import java.util.Scanner;
+
+/**
+ *
+ * 소수(에라토스테네스 체)
+ * - 체크하면서 걸러냄
+ *
+ * */
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n = in.nextInt();
+
+        PrimeNumber primeNumber = new PrimeNumber();
+        System.out.println(primeNumber.solution(n));
+
+    }
+
+    public int solution(int n) {
+
+        return 0;
+    }
+
+
+}
