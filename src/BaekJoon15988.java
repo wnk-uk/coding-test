0949b1b88b7a242f8374b29faf8161a4b50132bf
wnk-uk <dailycoding0211@gmail.com>
2024-05-21 18:27:24 +0900

    dp

--- src/BaekJoon15988.java
+++ src/BaekJoon15988.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class BaekJoon15988 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon15988 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        long[] arr = new long[1000001];
+        int[] tmp = new int[T];
+        arr[1] = 1;
+        arr[2] = 2;
+        arr[3] = 4;
+
+        long max = 0;
+        for (int i = 0; i < T; i++) {
+            int x = Integer.parseInt(br.readLine());
+            tmp[i] = x;
+            max = Math.max(x, max);
+        }
+
+        for (int i = 4; i <= max; i++) {
+            arr[i] = (arr[i - 1] + arr[i - 2] + arr[i - 3]) % 1000000009;
+        }
+
+        for (int x : tmp) {
+            System.out.println(arr[x]);
+        }
+
+
+
+    }
+
 }
--- src/BaekJoon15990.java
+++ src/BaekJoon15990.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class BaekJoon15990 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon15990 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        long max = 0L;
+        long mod = 1000000009;
+
+        int arr[] = new int[T];
+        for (int i = 0; i < T; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, arr[i]);
+        }
+
+        long[][] dp = new long[100001][4];
+        dp[1][1] = dp[2][2] = dp[3][3] = dp[3][2] = dp[3][1] = 1;
+
+        for (int i = 4; i <= max; i++) {
+            dp[i][1] = (dp[i - 1][2] + dp[i - 1][3]) % mod;
+            dp[i][2] = (dp[i - 2][1] + dp[i - 2][3]) % mod;;
+            dp[i][3] = (dp[i - 3][1] + dp[i - 3][2]) % mod;;
+        }
+
+        for (int x : arr) {
+            long answer = (dp[x][1] + dp[x][2] + dp[x][3]) % mod;
+            System.out.println(answer);
+        }
+    }
+
 }
